fb99def9362df927f346b0cb33b1748740e9ef12
zunerakkhan <zunerakhan6868@gmail.com>
2025-08-27 20:54:56 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,17 @@
+
+console.log("Hello world!");
+
+const s1 = {
+  name: "Zunera Khan",
+  age: 19,
+  course: "B.Tech in ECE",
+  marks: [85, 92, 78, 88, 95]
+};
+
+console.log("Student Details:");
+console.log("Name:", s1.name);
+console.log("Age:", s1.age);
+console.log("Course:", s1.course);
+console.log("Marks:", s1.marks);
+
+
